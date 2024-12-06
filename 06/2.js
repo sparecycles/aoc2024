fad0332b208d2b62fd625274663b7f5cfd0c8465
Adam Freidin <adam.freidin@gmail.com>
2024-12-06 09:08:22 -0800

    day 06 star 2

--- 06/2.js
+++ 06/2.js
@@ -1,7 +1,9 @@
-import chalk from 'chalk';
+import { Chalk } from 'chalk';
 import { readFileSync } from 'node:fs';
 const arg = process.argv[2];
 const input = arg == 'i' ? './input' : (arg ?? './sample');
+
+const chalk = new Chalk({ level: 2 });
 
 const lines = readFileSync(input, 'utf-8')
   .split('\n')
@@ -30,27 +32,19 @@ function dir(dx, dy) {
 
 const steps = lines.map((line) => line.map((_) => [_]));
 
-function searchTurn(guard, q) {
-  let mylines = lines.map((line) => line.map((c) => c));
-  const mysteps = steps.map((line) => line.map((_) => (q ? _.slice() : [])));
+function searchBlocker(guard) {
+  const mysteps = steps.map((line) => line.map((_) => _.slice()));
 
   const facing = dir(guard.dx, guard.dy);
 
-  Object.assign(guard, { dx: -guard.dy, dy: guard.dx });
-
   mysteps[guard.y][guard.x].push(facing);
-  mylines[guard.y][guard.x] = '%';
 
   while (step(guard)) {
     const facing = dir(guard.dx, guard.dy);
     if (mysteps[guard.y][guard.x].includes(facing)) {
-      //if (!steps[guard.y][guard.x].includes(facing) && depth > 0)
-      //  return searchTurn(guard, depth - 1);
-
-      return mylines;
+      return true;
     }
 
-    mylines[guard.y][guard.x] = '%';
     mysteps[guard.y][guard.x].push(facing);
   }
 }
@@ -73,59 +67,32 @@ function step(guard) {
       dy: guard.dx,
     });
 
-    const next = { ...guard };
-    if (step(next) && next.dx !== guard.dx) {
-      return false;
-    }
-
     return true;
-    //    return step(guard);
   }
 
   Object.assign(guard, { x: nx, y: ny });
   return true;
 }
 
-steps[guard.y][guard.x].push(dir(guard.dx, guard.dy));
-
 while (step(guard)) {
   const facing = dir(guard.dx, guard.dy);
   steps[guard.y][guard.x].push(facing);
   lines[guard.y][guard.x] = facing;
 
-  let search;
-  if (
-    lines[guard.y + guard.dy]?.[guard.x + guard.dx] === '.' &&
-    (search = searchTurn({
-      ...guard,
-    }))
-  ) {
-    const [ox, oy] = [guard.x + guard.dx, guard.y + guard.dy];
-    steps[oy][ox].push('o');
+  const [ox, oy] = [guard.x + guard.dx, guard.y + guard.dy];
+  if (lines[oy]?.[ox] === '.') {
+    lines[oy][ox] = '#';
 
-    if (searchTurn({ ...guard }, true)) {
-      console.log(
-        count +
-          '\n' +
-          search
-            .map((line, y) =>
-              line
-                .map((c, x) =>
-                  x == ox && y == oy
-                    ? chalk.green('o')
-                    : lines[y][x] === '#'
-                      ? chalk.blue(c)
-                      : steps[y][x].includes('o')
-                        ? chalk.red(c)
-                        : c,
-                )
-                .join(''),
-            )
-            .join('\n') +
-          '\n\n',
-      );
+    if (
+      searchBlocker({
+        ...guard,
+      })
+    ) {
+      steps[oy][ox].push('o');
       count++;
     }
+
+    lines[oy][ox] = '.';
   }
 }
 
