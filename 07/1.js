d9052f3a4402d6369cdbf44eb14d72f2f99d54fa
Adam Freidin <adam.freidin@gmail.com>
2024-12-07 08:15:28 -0800

    day 07

---
+++ 07/1.js
@@ -0,0 +1,40 @@
+import { readFileSync } from 'node:fs';
+const arg = process.argv[2];
+const input = arg == 'i' ? './input' : (arg ?? './sample');
+
+const lines = readFileSync(input, 'utf-8')
+  .split('\n')
+  .filter(Boolean)
+  .map((line) => line.split(':'))
+  .map(([target, nums]) => ({
+    target: Number(target),
+    nums: nums.trim().split(/\s+/).filter(Boolean).map(Number),
+  }));
+
+function check(target, current, ...nums) {
+  if (current > target) {
+    return false;
+  }
+
+  if (nums.length === 0) {
+    return current === target;
+  }
+
+  return (
+    checkProd(target, current, ...nums) || checkSum(target, current, ...nums)
+  );
+}
+
+function checkSum(target, current, next, ...nums) {
+  return check(target, current + next, ...nums);
+}
+
+function checkProd(target, current, next, ...nums) {
+  return check(target, current * next, ...nums);
+}
+
+const result = lines
+  .filter(({ target, nums }) => check(target, ...nums))
+  .reduce((sum, { target }) => sum + target, 0);
+
+console.log(result);
